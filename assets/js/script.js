7ea344d78e0e02a85e49a3b141ffb4d4bc5b0bf1
Guillaume Alves <guillaume.alves@netcourrier.com>
2022-06-26 10:28:43 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,18 @@ function loadNavbar() {
     $( "#navbar" ).load( "navbar.html" );
 }
 
+function displayHide() {
+    let list = document.getElementById("sidebar_films_list");
+    let arrow_down = document.getElementById("arrow_down");
+    let arrow_up = document.getElementById("arrow_up");
+    if (list.style.display === "none") {
+        list.style.display = "block";
+        arrow_down.style.display = "none";
+        arrow_up.style.display = "inline";
 
-    $('#show_sidebar_films').on(function () {
-        $('#sidebar_films_items').hide();
-    });
+    } else {
+        list.style.display = "none";
+        arrow_down.style.display = "inline";
+        arrow_up.style.display = "none";
+    }
+}
