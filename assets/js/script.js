95a9bfddf099f38c25cf0f284a214959f48cefb4
CecileDumas <101016714+CecileDumas@users.noreply.github.com>
2022-06-29 10:41:00 +0200

    modifs cécile...
    c'est un peu le bazar mais bon..

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,11 @@ function displayHide() {
         arrow_up.style.display = "none";
     }
 }
+
+
+function PopupImage(img) {
+	w=open("",'image','weigth=toolbar=no,scrollbars=no,resizable=yes, width=510, height=210');	
+	w.document.write("<HTML><BODY onblur=\"window.close();\"><IMG src='"+img+"'>");
+	w.document.write("</BODY></HTML>");
+	w.document.close();
+}
