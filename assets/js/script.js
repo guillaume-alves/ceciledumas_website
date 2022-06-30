3da43c19e75c24015d7f5f1fd2b95da5e4bec053
Guillaume Alves <guillaume.alves@netcourrier.com>
2022-06-30 23:52:48 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-function loadNavbarAndSidebar() {
+function loadNavbar() {
     $( "#navbar" ).load( "navbar.html" );
+}
+
+function loadSidebar() {
     $("#sidebar").load("sidebar.html");
 }
 
@@ -7,14 +10,14 @@ function displayHide(clickedList, arrowUpList, arrowDownList) {
     let list = document.getElementById(clickedList);
     let arrowDown = document.getElementById(arrowDownList);
     let arrowUp = document.getElementById(arrowUpList);
-    if (list.style.display === "none") {
-        list.style.display = "block";
-        arrowDown.style.display = "none";
-        arrowUp.style.display = "inline";
-    } else {
+    if (list.style.display === "block") {
         list.style.display = "none";
         arrowDown.style.display = "inline";
         arrowUp.style.display = "none";
+    } else {
+        list.style.display = "block";
+        arrowDown.style.display = "none";
+        arrowUp.style.display = "inline";
     }
 }
 
